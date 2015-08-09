73cc385c5326d330d43e85ee52ee1756f86f319c
lttazz99 <lttazz99@gmail.com>
2015-08-09 17:01:28 +0530

    adv04l accepted

--- SPOJ/src/adv04l/Main.java
+++ SPOJ/src/adv04l/Main.java
@@ -1,75 +1,53 @@
 package adv04l;
-//import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.util.ArrayList;
 import java.util.InputMismatchException;
 
 public class Main {
-	// static ArrayList<Long> fibo = new ArrayList<Long>();
 	static long[] fibo = new long[74];
 
 	public static void main(String[] args) throws NumberFormatException,
 			IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		InputReader in = new InputReader(System.in);
 		precompute();
-//		int t = Integer.parseInt(br.readLine());
 		int t = in.readInt();
 		long[] inp = new long[t];
 		for (int i = 0; i < t; i++)
-//			inp[i] = Long.parseLong(br.readLine());
 			inp[i] = in.readLong();
 
-		for (int i = 0; i < t; i++)
-			System.out.println(miles2kms(inp[i]));
-	}
-
-	private static long miles2kms(long l) {
-		if (l == 1)
-			return 2;
-		// int index = fibo.indexOf(l);
-		// if (index > -1) {
-		// return fibo.get(index + 1);
-		// }
-		for (int i = 1; i < 74; i++)
-			if (fibo[i] == l)
-				return fibo[i + 1];
-		long num;
-		long kms = 0;
-		while (l > 0) {
-			for (int i = 1; i < 74; i++) {
-				// num = fibo.get(i);
+		long kms = 0, num;
+		for (int j = 0; j < t; j++) {
+			if (inp[j] == 1) {
+				System.out.println(2);
+				continue;
+			}
+			kms = 0;
+			for (int i = 73; i >= 0; i--) {
 				num = fibo[i];
-				if (num > l) {
-					kms += num;
-					// l -= fibo.get(i - 1);
-					l -= fibo[i - 1];
-					break;
+				if (num <= inp[j]) {
+					kms += fibo[i + 1];
+					inp[j] -= num;
+					if (inp[j] < 1)
+						break;
 				}
 			}
+			System.out.println(kms);
 		}
-		return kms;
 	}
 
 	private static void precompute() {
 		long a0 = 1L, a1 = 1L, a2 = 0L;
-		// fibo.add(a0);
-		// fibo.add(a1);
 		fibo[0] = fibo[1] = 1L;
 		int i = 2;
 		while (a2 <= 1000000000000000L) {
 			a2 = a0 + a1;
-			// fibo.add(a2);
 			fibo[i++] = a2;
 			a0 = a1;
 			a1 = a2;
 		}
-		// fibo.remove(0);
-		fibo[0] = -1;
 	}
-	
+
 	private static class InputReader {
 		private InputStream stream;
 		private byte[] buf = new byte[1024];
@@ -126,7 +104,7 @@ public class Main {
 		public interface SpaceCharFilter {
 			public boolean isSpaceChar(int ch);
 		}
-		
+
 		public long readLong() {
 			int c = read();
 			while (isSpaceChar(c))
